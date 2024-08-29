aedbe2e521fcfcdcc65d9ce6a84f0ede7ae41ba0
luiscar2050 <luis3206162549@gmail.com>
2024-08-28 18:58:40 -0600

    getter y setter de clase cliente

--- src/edu/misena/factura/model/Cliente.java
+++ src/edu/misena/factura/model/Cliente.java
@@ -4,18 +4,22 @@ public class Cliente {
     private String nombre;
     private String nif; // Número de Identificación Fiscal
 
+    // Getter para el atributo nombre
     public String getNombre() {
         return nombre;
     }
 
+    // Setter para el atributo nombre
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    // Getter para el atributo nif
     public String getNif() {
         return nif;
     }
 
+    // Setter para el atributo nif
     public void setNif(String nif) {
         this.nif = nif;
     }
